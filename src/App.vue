ef7f908da88fd9e789551da6750e2429414713aa
dupeng <dupeng@mxnavi.com>
2022-06-28 14:35:30 +0800

    上传代码

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { until , invoke } from '@vueuse/core'
+import {ref} from 'vue'
+
+const source = ref(0)
+
+invoke(async () => {
+  await until(source).toBe(4)
+  console.log('满足条件了')
+}) 
+
+const clickedFn = () => {
+  source.value ++
+}
+</script>
+
+<template>
+ <div>{{source}}</div>
+  <button @click="clickedFn">
+    点击按钮
+  </button>
+</template>
